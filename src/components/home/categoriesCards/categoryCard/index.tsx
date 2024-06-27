8e18edac61996f562168bf503a1e55fcebc2c8c4
daramirezsu06 <daramirezsu06@gmail.com>
2024-06-27 12:20:36 -0500

    componente/home/categoriesCards created componet CategoryCard

---
+++ src/components/home/categoriesCards/categoryCard/index.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+const CategoryCard = ({ id, name, image, productCount }) => {
+  return (
+    <div className="rounded-md p-4 bg-white duration-500 hover:scale-105 shadow-md ">
+      <h2>{name}</h2>
+      <div className="relative w-52 h-52 rounded-full overflow-hidden mx-auto">
+        <Image
+          src={image}
+          alt={id}
+          layout="fill" // Usar layout fill para llenar el contenedor
+          objectFit="cover" // Ajustar la imagen para cubrir el contenedor
+          className="rounded-full"
+        />
+      </div>
+      <p>Cantidad de productos: {productCount}</p>
+    </div>
+  );
+};
+
+export default CategoryCard;
