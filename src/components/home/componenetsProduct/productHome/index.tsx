96178c4c45f55c0d7e21eb0db20733f14ad9dd8c
Mauricio Jourdan <jourdanmauricio@gmail.com>
2024-06-29 01:53:15 -0300

    fix(Api/preaload-data products): Change preload-data

--- src/components/home/componenetsProduct/productHome/index.tsx
+++ src/components/home/componenetsProduct/productHome/index.tsx
@@ -1,13 +1,7 @@
-import getProducts from "@/utils/api/products/getProducts";
 import ProductCard from "../productCard";
 
-const ProductContentHome = async () => {
-  const getData = async () => {
-    const data = await getProducts();
-    return data;
-  };
+const ProductContentHome = ({ products }) => {
 
-  const data = await getData();
   return (
     <div className="mb-16">
       <h1 className="text-3xl font-bold text-center mb-9">
@@ -16,7 +10,7 @@ const ProductContentHome = async () => {
       <div className="flex justify-center items-center gap-4">
         <div className="text-3xl font-bold ">&lt;</div>
         <div className="flex gap-4 ">
-          {data.map((product) => (
+          {products.map((product) => (
             <ProductCard key={product.id} {...product} />
           ))}
         </div>
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,13 +1,23 @@
-import ProductContentHome from "../componenetsProduct/productHome";
+import ProductContentHome from "./componenetsProduct/productHome";
 import BrandCards from "./brandCards";
 import CategoriesCards from "./categoriesCards";
+import getCategories from '@/utils/api/categories/getCategories';
+import getCategory from '@/utils/api/categories/getCategory';
+import getBrands from '@/utils/api/brands/getBrands';
 
-const Home = () => {
+const Home = async () => {
+
+  const categories = await getCategories()
+  const catFeatured = categories.find(el => el.name === "Destacados")
+  const featuredProducts = await getCategory(catFeatured.id)
+
+  const brands = await getBrands();
+
   return (
     <div>
-      <CategoriesCards />
-      <BrandCards />
-      <ProductContentHome />
+      <CategoriesCards categories={categories} />
+      <BrandCards brands={brands} />
+      <ProductContentHome products={featuredProducts.products} />
     </div>
   );
 };
