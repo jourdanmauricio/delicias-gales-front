2077bd14516cb8f12a5603c73147d0f309b0f10a
Mauricio Jourdan <jourdanmauricio@gmail.com>
2024-07-10 17:22:43 -0300

    Feat(dashboard/employee initial): Add initial employee dashboard

---
+++ src/components/dashboards/employee/home/index.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className='text-slate-800'>
+      <div className=''>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+      <div>Home</div>
+    </div>
+  )
+}
+export default Home
